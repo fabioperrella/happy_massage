21893740c293ff8b482fa8e20c91050985469c6f
Fabio Perrella <fabio.perrella@gmail.com>
2016-08-22 21:23:33 -0300

    [add_to_calendar] creating 3 masseurs in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,3 @@
-Masseur.create!(name: 'Massagista1', email: 'massagista@locaweb.com.br')
+Masseur.find_or_create_by!(name: 'Massagista1', email: 'massagista1@locaweb.com.br')
+Masseur.find_or_create_by!(name: 'Massagista2', email: 'massagista2@locaweb.com.br')
+Masseur.find_or_create_by!(name: 'Massagista3', email: 'massagista3@locaweb.com.br')
