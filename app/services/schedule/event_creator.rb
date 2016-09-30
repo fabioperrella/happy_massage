69966fefc49eb0e3d83ca6d061c72a802041f909
Fabio Perrella <fabio.perrella@locaweb.com.br>
2016-09-29 22:36:05 -0300

    [add_to_calendar] replaced hardcoded variables by dynamic variables

--- app/services/schedule/event_creator.rb
+++ app/services/schedule/event_creator.rb
@@ -2,26 +2,24 @@ require 'icalendar/tzinfo'
 
 module Schedule
   class EventCreator
-    TZID = "America/Sao_Paulo"
-
     def create_event(massage:)
       cal = Icalendar::Calendar.new
       cal.append_custom_property("METHOD", "REQUEST")
 
       cal.timezone do |t|
-        t.tzid = TZID
+        t.tzid = tzid
 
         t.standard do |s|
-          s.tzoffsetfrom = "-0200"
-          s.tzoffsetto   = "-0300"
-          s.tzname       = "BRT"
-          s.dtstart      = "20160221T020000"
+          s.tzoffsetfrom = daylight_previous_period_offset
+          s.tzoffsetto   = time_now.formatted_offset(false)
+          s.tzname       = time_now.strftime('%Z')
+          s.dtstart      = daylight_current_period_start
         end
       end
 
       cal.event do |e|
-        e.dtstart     = Icalendar::Values::DateTime.new massage.timetable, tzid: TZID
-        e.dtend       = Icalendar::Values::DateTime.new massage.timetable + 15.minutes, tzid: TZID
+        e.dtstart     = Icalendar::Values::DateTime.new massage.timetable, tzid: tzid
+        e.dtend       = Icalendar::Values::DateTime.new massage.timetable + 15.minutes, tzid: tzid
         e.summary     = t(:summary)
         e.description = t(:description)
         e.ip_class    = "PUBLIC"
@@ -34,7 +32,8 @@ module Schedule
           "CN"       => massage.user_name,
           "X-NUM-GUESTS" => 1
         }
-        attendee_value = Icalendar::Values::CalAddress.new("MAILTO:#{massage.user_email}", attendee_params)
+        attendee_value = Icalendar::Values::CalAddress
+          .new("MAILTO:#{massage.user_email}", attendee_params)
         e.append_attendee(attendee_value)
       end
 
@@ -45,6 +44,25 @@ module Schedule
 
     def t(key)
       I18n.t("services.event_creator.#{key}")
+    end
+
+    def tzid
+      @tzid ||= ActiveSupport::TimeZone::MAPPING[Time.zone.name]
+    end
+
+    def daylight_current_period_start
+      Time.zone.tzinfo.current_period
+          .utc_start.strftime('%Y%m%dT%H%M%S')
+    end
+
+    def time_now
+      @time_now ||= Time.zone.now
+    end
+
+    def daylight_previous_period_offset
+      (Time.zone.tzinfo.current_period.utc_start - 1.day)
+        .in_time_zone
+        .formatted_offset(false)
     end
   end
 end
