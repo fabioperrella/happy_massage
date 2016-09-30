7ed0c5b5342106232c0ac3d4ea8eba3caf93dc69
Fabio Perrella <fabio.perrella@locaweb.com.br>
2016-09-29 22:45:42 -0300

    [add_to_calendar] refactoring

--- app/services/schedule/event_creator.rb
+++ app/services/schedule/event_creator.rb
@@ -3,44 +3,54 @@ require 'icalendar/tzinfo'
 module Schedule
   class EventCreator
     def create_event(massage:)
-      cal = Icalendar::Calendar.new
+      @cal = Icalendar::Calendar.new
+      @massage = massage
       cal.append_custom_property("METHOD", "REQUEST")
 
+      set_timezone
+      set_event
+
+      cal.to_ical
+    end
+
+    private
+
+    attr_accessor :cal, :massage
+
+    def set_timezone
       cal.timezone do |t|
         t.tzid = tzid
 
         t.standard do |s|
           s.tzoffsetfrom = daylight_previous_period_offset
-          s.tzoffsetto   = time_now.formatted_offset(false)
-          s.tzname       = time_now.strftime('%Z')
-          s.dtstart      = daylight_current_period_start
+          s.tzoffsetto = time_now.formatted_offset(false)
+          s.tzname = time_now.strftime('%Z')
+          s.dtstart = daylight_current_period_start
         end
       end
+    end
 
+    def set_event
       cal.event do |e|
-        e.dtstart     = Icalendar::Values::DateTime.new massage.timetable, tzid: tzid
-        e.dtend       = Icalendar::Values::DateTime.new massage.timetable + 15.minutes, tzid: tzid
-        e.summary     = t(:summary)
+        e.dtstart = massage_dt_start
+        e.dtend = massage_dt_end
+        e.summary = t(:summary)
         e.description = t(:description)
-        e.ip_class    = "PUBLIC"
+        e.ip_class = "PUBLIC"
 
         attendee_params = {
-          "CUTYPE"   => "INDIVIDUAL",
-          "ROLE"     => "REQ-PARTICIPANT",
+          "CUTYPE" => "INDIVIDUAL",
+          "ROLE" => "REQ-PARTICIPANT",
           "PARTSTAT" => "NEEDS-ACTION",
-          "RSVP"     => "TRUE",
-          "CN"       => massage.user_name,
+          "RSVP" => "TRUE",
+          "CN" => massage.user_name,
           "X-NUM-GUESTS" => 1
         }
         attendee_value = Icalendar::Values::CalAddress
           .new("MAILTO:#{massage.user_email}", attendee_params)
         e.append_attendee(attendee_value)
       end
-
-      cal.to_ical
     end
-
-    private
 
     def t(key)
       I18n.t("services.event_creator.#{key}")
@@ -63,6 +73,17 @@ module Schedule
       (Time.zone.tzinfo.current_period.utc_start - 1.day)
         .in_time_zone
         .formatted_offset(false)
+    end
+
+    def massage_dt_start
+      Icalendar::Values::DateTime.new massage.timetable, tzid: tzid
+    end
+
+    def massage_dt_end
+      Icalendar::Values::DateTime.new(
+        massage.timetable + ScheduleSettings.massage_duration,
+        tzid: tzid
+      )
     end
   end
 end
--- spec/services/schedule/event_creator_spec.rb
+++ spec/services/schedule/event_creator_spec.rb
@@ -4,7 +4,6 @@ describe Schedule::EventCreator do
 
     subject(:create_event) { described_class.new.create_event(massage: massage) }
 
-    it { expect(create_event).to eq "111" }
     it { expect(create_event).to include "METHOD:REQUEST" }
     it { expect(create_event).to include "BEGIN:VTIMEZONE" }
     it { expect(create_event).to include "BEGIN:VCALENDAR" }
